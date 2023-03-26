c7f98eb54dea52e4197b205c83d49db4dbb45148
DanxDevelop <dfm@yoyoglobal.com>
2023-03-26 17:49:02 +0200

    ..

--- main.go
+++ main.go
@@ -7,14 +7,29 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 type item struct {
+	Id    int32
 	Name  string
 	Point int32
+}
+
+type update struct {
+	Id            string
+	PointToRemove int32
+}
+
+func (u update) IdAsInt() int {
+	i, err := strconv.Atoi(u.Id)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
 
 func main() {
@@ -27,9 +42,38 @@ func main() {
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "dashboard.html", gin.H{
-			"content": "This is an index page...",
-			"items":   readDatafileToStruct(),
+			"items": readDatafileToStruct(),
 		})
+	})
+
+	router.GET("/admin", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "admin.html", gin.H{
+			"items": readDatafileToStruct(),
+		})
+	})
+
+	router.POST("/update", func(c *gin.Context) {
+		fmt.Println("Update Received")
+		var updateItem update
+		if err := c.BindJSON(&updateItem); err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(updateItem)
+		items := readDatafileToStruct()
+		for i, el := range items {
+			if el.Id == int32(updateItem.IdAsInt()) {
+				fmt.Println("Found Match!")
+				fmt.Println("Points to remove", updateItem.PointToRemove)
+				fmt.Println("Points Before remove", el.Point)
+				newPoints := el.Point + updateItem.PointToRemove
+				items[i].Point = newPoints
+				fmt.Println(items[i].Point)
+
+			}
+		}
+		updateData(items)
+		return
 	})
 
 	router.Run("localhost:8080")
@@ -42,7 +86,7 @@ func readDatafileToStruct() []item {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("Successfully Opened users.json")
+	fmt.Println("Successfully Opened data.json")
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
@@ -53,4 +97,18 @@ func readDatafileToStruct() []item {
 
 	json.Unmarshal(byteValue, &result)
 	return result
+}
+
+func updateData(items []item) {
+	j, err := json.Marshal(items)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+	} else {
+		f, _ := os.OpenFile("data.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		defer f.Close()
+
+		f.WriteString(string(j))
+		fmt.Println("Updated json file", string(j))
+	}
+
 }
